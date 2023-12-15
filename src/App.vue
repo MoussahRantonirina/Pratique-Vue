257e53a63da95db5b33865c86b7c98f5c624751a
Moussah <rantomoussah@gmail.com>
2023-12-15 12:48:22 +0300

    Latest Update

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Last name</th>
+                    <th>Birth date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <TrowTable @manova-data="ovayLeData" v-for="p in pers" :key="p.id" :persItem = "p" />
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+import TrowTable from './components/TrowTable.vue';
+export default {
+    data() {
+        return {
+            pers : [
+                {
+                    id : 1,
+                    name : "Rakoto",
+                    lname : "Jean Baptiste",
+                    birth : "2000-05-01"
+                },
+                {
+                    id : 2,
+                    name : "Rakoto dddd",
+                    lname : "Jean Baptiste",
+                    birth : "2000-05-01"
+                },
+                {
+                    id : 3,
+                    name : "Rakoto dddd hhhh",
+                    lname : "Jean Baptiste",
+                    birth : "2000-05-01"
+                }
+            ]
+        }
+    },
+    components:{TrowTable},
+    methods: {
+        ovayLeData(data){
+            console.log(data)
+        }
+    }
+}
+</script>
---
+++ src/components/TrowTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <tr>
+        <td><input @keypress="makeS($event)" :disabled="isEdit ? false : true" type="text" v-model="persItem.name"></td>
+        <td>{{ persItem.lname }}</td>
+        <td>{{ persItem.birth }}</td>
+        <td>
+            <div>
+                <button @click="isEdit = true">Edit</button>
+                <button>Delete</button>
+            </div>
+        </td>
+    </tr>
+</template>
+<script>
+    export default{
+        props : ['persItem'],
+        data(){
+            return {
+                isEdit : false
+            }
+        },
+        methods : {
+            makeS(e){
+                //console.log(e)
+                if(e.key == 'Enter'){
+                    //console.log('manova donnée')
+                    this.$emit('manova-data', this.persItem)
+                }
+            }
+        }
+    }
+</script>
